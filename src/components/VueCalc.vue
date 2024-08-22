535a8baeb7aa11e20a465d089adb7759961925ac
Anastasuaya <anastasuaya.bashlykova09@gmail.com>
2024-08-22 21:05:49 +0300

    .

--- src/components/VueCalc.vue
+++ src/components/VueCalc.vue
@@ -7,29 +7,38 @@
       <div style="font-size: 22px; height: 12px; text-align: left; padding: 10px;">{{ calcCurrentStr }}</div>
     </div>
     <div style="display: flex;">
-      <button @click="calcEvents('1')">1</button>
-      <button @click="calcEvents('2')">2</button>
-      <button @click="calcEvents('3')">3</button>
+      <div>
+        <div style="display: flex;">
+          <button @click="calcEvents('1')">1</button>
+          <button @click="calcEvents('2')">2</button>
+          <button @click="calcEvents('3')">3</button>
+        </div>
+        <div style="display: flex;">
+          <button @click="calcEvents('4')">4</button>
+          <button @click="calcEvents('5')">5</button>
+          <button @click="calcEvents('6')">6</button>
+        </div>
+        <div style="display: flex;">
+          <button @click="calcEvents('7')">7</button>
+          <button @click="calcEvents('8')">8</button>
+          <button @click="calcEvents('9')">9</button>
+        </div>
+        <div style="display: flex; justify-content: center;">
+          <button @click="calcEvents('0')">0</button>
+        </div>
+      </div>
+      <div style="display: flex; flex-direction: column;">
+        <button @click="calcEvents('+')">+</button>
+        <button @click="calcEvents('*')">*</button>
+        <button @click="calcEvents('-')">-</button>
+      </div>
+      <div style="display: flex; flex-direction: column;">
+        <button @click="calcEvents('=')">=</button>
+        <button @click="calcEvents('/')">/</button>
+      </div>
     </div>
     <div style="display: flex;">
-      <button @click="calcEvents('4')">4</button>
-      <button @click="calcEvents('5')">5</button>
-      <button @click="calcEvents('6')">6</button>
-    </div>
-    <div style="display: flex;">
-      <button @click="calcEvents('7')">7</button>
-      <button @click="calcEvents('8')">8</button>
-      <button @click="calcEvents('9')">9</button>
-    </div>
-    <div style="display: flex;">
-      <button @click="calcEvents('0')">0</button>
-      <button @click="calcEvents('+')">+</button>
-      <button @click="calcEvents('-')">-</button>
-    </div>
-    <div style="display:flex">
-      <button @click="calcEvents('/')">/</button>
-      <button @click="calcEvents('*')">*</button>
-      <button @click="calcEvents('=')">=</button>
+
     </div>
   </div>
 </template>
@@ -41,8 +50,8 @@ const calc = ref()
 const calcStr = ref('')
 const calcCurrentStr = ref('')
 
-const calcKeyboard = (e:KeyboardEvent) => {
-  if (['1','2','3','4','5','6','7','8','9','0','+','-','/','*','=','Enter'].includes(e.key)) {
+const calcKeyboard = (e: KeyboardEvent) => {
+  if (['1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '+', '-', '/', '*', '=', 'Enter'].includes(e.key)) {
     let key = e.key
     if (key == ',') key = '.'
     if (key == 'Enter') key = '='
@@ -57,7 +66,7 @@ onBeforeUnmount(() => {
   document.removeEventListener('keydown', calcKeyboard)
 })
 
-const calcEvents = (char:string) => {
+const calcEvents = (char: string) => {
   if (char == '.') {
     if (calcCurrentStr.value.includes('.')) return
   }
